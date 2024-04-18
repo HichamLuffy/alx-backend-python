fef4f37c7223c1a7fc1ec72e8d5de9d53abfdc6f
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 11:57:17 +0100

    finish ? 200%?

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" Type Checking """
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List[int]:
+    """type checking advenced"""
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[int,...] = (12, 72, 91)
+
+zoom_2x: List[int] = zoom_array(array)
+
+zoom_3x: List[int] = zoom_array(array, int(3.0))
