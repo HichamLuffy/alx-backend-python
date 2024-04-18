afea300587b95286179df5a2527ca86e36330fa0
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 11:25:09 +0100

    string and int float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-"""Type-annotated f"""
+"""string and int/float to tuple"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """string and int/float to tuple"""
+    return (k, v**2)
