98c20065622252e58392e47c3423ab82a3ffacad
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 11:46:04 +0100

    advenced duck typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""duck typing"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ duck typing"""
+    if lst:
+        return lst[0]
+    else:
+        return None
