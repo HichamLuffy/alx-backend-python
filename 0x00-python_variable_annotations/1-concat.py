8fa49fdc3fd523e7c3e121106e5444c3a671b610
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 10:50:38 +0100

    cancat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-def show(value: str, excitement: int = 10) -> None:
-    print(value + value[-1] * excitement)
+""" conccat """
 
 
-show("Hello World")
+def concat(str1: str, str2: str) -> str:
+    """concat function"""
+    return str1 + str2
