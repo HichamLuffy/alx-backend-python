5505e6a157fdacbb620898f86a9709bc1e82d606
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 11:52:01 +0100

    safely get value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """typevar :TypeVar is used to create a generic type
+    that can change based on the input. This function looks
+    up key in dct and returns the corresponding
+    value or the default if the key is not found."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
