ca3e5aa80785c235b17b73591857c62a4faabca7
HichamLuffy <imalexvssteve2020@gmail.com>
2024-04-18 11:32:42 +0100

    callable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" complex types functions """
+from typing import Callable
+
+
+def make_multiplier(multiplayer: float) -> Callable[[float], float]:
+    return lambda x: multiplayer ** 2
