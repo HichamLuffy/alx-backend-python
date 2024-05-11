7a019adcfe2100bdeb6b126fb0e7192a9a9f5cbc
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-11 20:59:20 +0100

    add typing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Any, Tuple, Dict
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -12,6 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(
+        self, nested_map: Dict[str, Any], path: Tuple[str], expected: Any) -> None:
         """ FUNC """
         self.assertEqual(access_nested_map(nested_map, path), expected)
