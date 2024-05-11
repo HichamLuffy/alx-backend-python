246c957dca6df3d87697a39cbc37cb5a03a3ed63
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-11 21:04:57 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,13 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """ FUNC """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(
+        self, nested_map: Dict[str, Any], path: Tuple[str]) -> None:
+        """FUNC """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
