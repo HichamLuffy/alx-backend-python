73f4c91d544948f795849120cbe6476294c7b2a3
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-12 16:11:24 +0100

    task 4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""test client"""
+from unittest.mock import patch, Mock, PropertyMock
+from parameterized import parameterized, parameterized_class
+from client import GithubOrgClient
+import unittest
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """test github org client"""
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+        """test org"""
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client, {"payload": True})
+        url = f"https://api.github.com/orgs/{org_name}"
+        mock_get_json.assert_called_once_with(url)
